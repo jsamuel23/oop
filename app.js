0ad1713fcd26f0a4c62e98395cbe42f37fdd1bec
jan.vogelainen@khk.ee <jan.vogelainen@khk.ee>
2019-11-12 15:05:20 +0200

    Document object ja tema omadused

--- app.js
+++ app.js
@@ -1,44 +1,19 @@
-// window objects / methods / properties
+// document
 
-// alert
-// window.alert('Tere hommikust');
-// alert('Tere hommikust');
+let sisu;
 
-// propt
-// const sisend = prompt('Sisesta oma nimi');
-// alert('Tere Hommikust, ' + sisend + '!');
+sisu = document; // sisu html-is
+sisu = document.all; //html tagid kasutamisjärjekorras
+sisu = document.all[2]; // konkreetne html tag
+sisu = document.all.length; // palju on html tagi kasutusel
 
-// confirm
-// if(confirm('Kas oled nõus?')){
-//    console.log('jah');
-// } else{
-//    console.log('ei');
-// }
+// Documendi omadused
+sisu = document.head; // head sisu
+sisu = document.body; // body sisu
+sisu = document.doctype; // doctype
+sisu = document.domain;
+sisu = document.URL;
+sisu = document.characterSet;
+sisu = document.contentType;
 
-// välimõõdud
-// let valikorgus = window.outerHeight;
-// let valilaius = window.outerWidth;
-// console.log(valilaius);
-
-// sisemõõdud
-// let siseKorgua = window.innerHeight
-// let siselaius = window.innerWidth
-
-// console.log(siselaius);
-
-// scroll
-// scrollY = window.scrollY
-// scrollX = window.scrollX
-// console.log(scrollY);
-
-//location objekt
-// let objekt = window.location;
-// let hostname = window.location.hostname;
-// let port = window.location.port;
-// let href = window.location.href;
-// let search = window.location.search;
-
-// redirect
-// window.location.href = 'https://khk.ee';
-// reload
-// window.location.reload();
+console.log(sisu);
