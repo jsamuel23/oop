661e65008521a8014eea4df5b3a4091d76ae9343
jan.vogelainen@khk.ee <jan.vogelainen@khk.ee>
2019-11-12 14:43:05 +0200

    täiendatud index.html materializecss abil

---
+++ app.js
@@ -0,0 +1,44 @@
+// window objects / methods / properties
+
+// alert
+// window.alert('Tere hommikust');
+// alert('Tere hommikust');
+
+// propt
+// const sisend = prompt('Sisesta oma nimi');
+// alert('Tere Hommikust, ' + sisend + '!');
+
+// confirm
+// if(confirm('Kas oled nõus?')){
+//    console.log('jah');
+// } else{
+//    console.log('ei');
+// }
+
+// välimõõdud
+// let valikorgus = window.outerHeight;
+// let valilaius = window.outerWidth;
+// console.log(valilaius);
+
+// sisemõõdud
+// let siseKorgua = window.innerHeight
+// let siselaius = window.innerWidth
+
+// console.log(siselaius);
+
+// scroll
+// scrollY = window.scrollY
+// scrollX = window.scrollX
+// console.log(scrollY);
+
+//location objekt
+// let objekt = window.location;
+// let hostname = window.location.hostname;
+// let port = window.location.port;
+// let href = window.location.href;
+// let search = window.location.search;
+
+// redirect
+// window.location.href = 'https://khk.ee';
+// reload
+// window.location.reload();
