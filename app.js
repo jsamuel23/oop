539931882d80d531a061153d18c76acb2492107a
jan.vogelainen@khk.ee <jan.vogelainen@khk.ee>
2019-11-12 15:37:19 +0200

    Sisu muutmine

--- app.js
+++ app.js
@@ -10,4 +10,10 @@ document.getElementById('title').style.background = '#333';
 sisu.style.color = '#fff';
 sisu.style.padding = '5px';
 // sisu.style.display = 'none';
+
+// sisu muutmine
+sisu.textContent = 'Minu ülesandend';
+sisu.innerText = 'Just minu ülesanne';
+sisu.innerHTML = '<span style="color:green">Vabad ülesanded</span>';
+
 console.log(sisu);
