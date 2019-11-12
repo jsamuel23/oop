fadd7aad6b8989481195e2c1ad45b9150d924392
jan.vogelainen@khk.ee <jan.vogelainen@khk.ee>
2019-11-12 15:11:35 +0200

    Document forms omadused

--- app.js
+++ app.js
@@ -16,4 +16,11 @@ sisu = document.URL;
 sisu = document.characterSet;
 sisu = document.contentType;
 
+// formi andmed
+sisu = document.forms; // kõik vormid
+sisu = document.forms[0]; // esimene vorm
+sisu = document.forms[0].id; // esimese vormi id
+sisu = document.forms[0].method; // vormi töötlus meetod
+sisu = document.forms[0].action;
+
 console.log(sisu);
