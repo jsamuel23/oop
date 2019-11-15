aabe4e2d9d889b2c1f5b342f71c1725544ea2319
jan.vogelainen@khk.ee <jan.vogelainen@khk.ee>
2019-11-15 13:07:17 +0200

    mitme elemendi töötlus + näide + nimekiri

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 // document meetodid
-// single element selector
-
-sisu = document.querySelector('#title'); // id järgi
-sisu = document.querySelector('.card-title'); // klassi järgi
-sisu = document.querySelector('h5'); // elemendi järgi
-
-// stiili muutmine
-
-document.querySelector('li').style.color = 'red';
-document.querySelector('ul li').style.color = 'blue';
-document.querySelector('li:last-child').style.color = 'green';
-document.querySelector('li:nth-child(2)').style.color = 'orange';
-
+// multiply element selector
+let sisu = document.getElementsByClassName('collection-item');
 console.log(sisu);
+console.log(sisu[0]);
+sisu[0].style.color = 'orange';
+sisu[2].textContent = 'Õpi arvutivõrgud';
+
+//querySelector +klassi nime järgi
+let nimekirjaElemendid = document.querySelector('ul').getElementsByClassName('collection-item');
+
+//elemendi nime järgi
+nimekirjaElemendid = document.getElementsByTagName('li');
+
+// teisendame HTMLcollection massiiviks (array)
+nimekirjaElemendid = Array.from(nimekirjaElemendid);
+nimekirjaElemendid.reverse();
+
+// massiivi läbimine tsükliga
+for(let i = 0; i < 3; i++){
+console.log(nimekirjaElemendid[i].className);
+}
+
+ // forEach
+ nimekirjaElemendid.forEach(function(element){
+     console.log(element);
+     element.style.background = '#add';
+ });
+
+ let nimekiriPaaritu = document.querySelectorAll('li:nth-child(odd)');
+ console.log(nimekiriPaaritu);
+
+ let nimekiriPaaris = document.querySelectorAll('li:nth-child(even)');
+ console.log(nimekiriPaaris);
+ 
